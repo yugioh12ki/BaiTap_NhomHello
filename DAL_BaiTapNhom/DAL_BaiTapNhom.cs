d4feaf7bb167fff379635221157b27eecdab8ef2
Ezom <everyonebody440@gmail.com>
2024-10-10 22:18:53 +0700

    #

---
+++ DAL_BaiTapNhom/DAL_BaiTapNhom.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO_BaiTapNhom;
+
+namespace DAL_BaiTapNhom
+{
+    internal class DAL_BaiTapNhom
+    {
+
+    }
+}
