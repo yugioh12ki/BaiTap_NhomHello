b82e75ab608514778b6a43b516d60a040d433bb1
EreMika <143042488+PieckFingerr@users.noreply.github.com>
2024-10-11 19:14:50 +0700

    thêm comment 4.5 -> 4.7

--- BLL_BaiTapNhom/BLL_BaiTapNhom.cs
+++ BLL_BaiTapNhom/BLL_BaiTapNhom.cs
@@ -9,6 +9,7 @@ namespace BLL_BaiTapNhom
 {
     internal class BLL_BaiTapNhom
     {
+        // 4.5
         public void XuatDSDeTaiCoGVHD(List<DTO_cDeTai> dsDeTai)
         {
             bool checked = false;
@@ -20,18 +21,19 @@ namespace BLL_BaiTapNhom
                     {
                         XuatDeTai(dsDeTai[i]);  // Ai làm phần xuất danh sách đề tài thì viết tên hàm giống như này
                         checked = true;
-                        }
                     }
-                    if (checked == false) {
-                        throw new Exception("Các đề tài trong danh sách không có GVHD");
-                    }
-                } catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+                }
+                if (checked == false) {
+                    throw new Exception("Các đề tài trong danh sách không có GVHD");
+                }
+            } catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
             }
         }
-
+    
+        // 4.6
         public void CapNhapKinhPhi(List<DTO_cDeTai> dsDeTai)
         {
             for (int i = 0; i < dsDeTai.Count; i++)
@@ -39,7 +41,8 @@ namespace BLL_BaiTapNhom
                 dsDeTai[i].KinhPhi *= 10 %;
             }
         }
-
+    
+        // 4.7
         public void XuatDSDeTaiTren10TR(List<DTO_cDeTai> dsDeTai)
         {
             for (int i = 0; i < dsDeTai.Count; i++)
