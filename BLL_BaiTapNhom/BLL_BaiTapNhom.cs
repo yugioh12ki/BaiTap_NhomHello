0af6df9746c43ce65f1a456f4fcc48461cc91c1a
PieckFingerr <vinhgiachau@gmail.com>
2024-10-11 14:25:16 +0700

    4.5 -> 4.7

--- BLL_BaiTapNhom/BLL_BaiTapNhom.cs
+++ BLL_BaiTapNhom/BLL_BaiTapNhom.cs
@@ -3,11 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DTO_BaiTapNhom
 
 namespace BLL_BaiTapNhom
 {
     internal class BLL_BaiTapNhom
     {
+        public void XuatDSDeTaiCoGVHD(List<DTO_cDeTai> dsDeTai)
+        {
+            bool checked = false;
+            try
+            {
+                for (int i = 0; i < dsDeTai.Count; i++)
+                {
+                    if (dsDeTai[i].HoTenGV != null)
+                    {
+                        XuatDeTai(dsDeTai[i]);  // Ai làm phần xuất danh sách đề tài thì viết tên hàm giống như này
+                        checked = true;
+                        }
+                    }
+                    if (checked == false) {
+                        throw new Exception("Các đề tài trong danh sách không có GVHD");
+                    }
+                } catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            }
+        }
+
+        public void CapNhapKinhPhi(List<DTO_cDeTai> dsDeTai)
+        {
+            for (int i = 0; i < dsDeTai.Count; i++)
+            {
+                dsDeTai[i].KinhPhi *= 10 %;
+            }
+        }
+
+        public void XuatDSDeTaiTren10TR(List<DTO_cDeTai> dsDeTai)
+        {
+            for (int i = 0; i < dsDeTai.Count; i++)
+            {
+                if (dsDeTai[i].KinhPhi > 10)
+                {
+                    XuatDeTai(dsDeTai[i]);
+                }
+            }
+        }
 
     }
 }
